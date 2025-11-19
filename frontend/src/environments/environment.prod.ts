388d9d5221de2f4ebe378025adbcca2998328ada
Your Name <hreddyjulakanti007@gmail.com>
2025-11-19 18:50:12 +0530

    Remove environment.prod.ts - localhost-only project

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:8081',
-  keycloak: {
-    url: 'http://localhost:8090',
-    realm: 'promocode',
-    clientId: 'promocode-frontend'
-  }
-};
